46ae28786ce7c9587c29534acfbec52e43a2f62a
Björn Böing <b.boeing@online.de>
2024-10-25 08:08:34 +0200

    Change createNewPost signature to build post object in function

--- src/domain/post.js
+++ src/domain/post.js
@@ -1,12 +1,18 @@
 import {createPost, getAllPosts} from '../api/post.js';
 
-export const createNewPost = async (post) => {
+export const createNewPost = async (postFormData, userId) => {
     const allExistingPosts = await getAllPosts()
 
-    const titleAlreadyExists = !!allExistingPosts.find(existingPost => existingPost.title === post.title)
+    const titleAlreadyExists = !!allExistingPosts.find(existingPost => existingPost.title === postFormData.title)
 
     if (titleAlreadyExists) {
         throw new Error('Title already exists')
+    }
+
+    const post = {
+        title: postFormData.title,
+        body: postFormData.body,
+        userId: userId,
     }
 
     return createPost(post)
