912ee34233c3f983f2151846b2a6d5fc05824214
Björn Böing <b.boeing@online.de>
2024-10-31 11:42:49 +0100

    Add postService.js

---
+++ src/domain/postService.js
@@ -0,0 +1,43 @@
+import {createPost, getAllPosts, getPost, getPostsOfUser, updatePost} from '../api/post.js';
+
+class Post {
+    constructor(postData) {
+        this.id = postData.id;
+        this.title = postData.title;
+        this.body = postData.body;
+        this.userId = postData.userId;
+    }
+}
+
+export class PostService {
+    constructor() {
+        this.posts = [];
+    }
+
+    async loadAllPosts() {
+        this.posts = await getAllPosts();
+
+        return this.posts;
+    }
+
+    async loadPost(id) {
+        return getPost(id);
+    }
+
+    async createPost(post) {
+        return createPost(post);
+    }
+
+    async updatePost(postId, title, body, userId) {
+        if (!title && !body) {
+            // Nothing to update
+            return
+        }
+
+        return updatePost(postId, title, body, userId);
+    }
+
+    async getPostsOfUser(userId) {
+        return getPostsOfUser(userId);
+    }
+}
