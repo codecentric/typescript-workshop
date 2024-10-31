04cffb87523d12693668ae4993ae80a40c2493ef
Björn Böing <b.boeing@online.de>
2024-10-31 10:23:49 +0100

    Extract userService.js

--- src/domain/user.js
+++ src/domain/user.js
@@ -1,5 +1,4 @@
 import {getPostsOfUser} from '../api/post.js';
-import {loadAllUsers, loadUser} from '../api/user.js';
 
 export const getUserRank = async (userId) => {
     const postAmount = (await getPostsOfUser(userId)).length
@@ -10,53 +9,6 @@ export const getUserRank = async (userId) => {
         return "Beginner"
     } else {
         return "Advanced"
-    }
-}
-
-export class UserService {
-    constructor() {
-        this.users = []
-    }
-
-    async loadAllUsers() {
-        this.users = await loadAllUsers();
-
-        this.users = this.users.map(user => {
-            user.type = !!user.company ? "EXTERNAL" : "INTERNAL"
-        });
-
-        return this.users
-    }
-
-    async loadUser(id) {
-        return loadUser(id)
-    }
-
-    async getUserRank(userId) {
-        if (!this.users.includes(userId)) {
-            const user = await this.loadUser(userId)
-
-            if (!user) {
-                throw new Error(`User with ID ${userId} does not exist`)
-            }
-            this.users.push(user)
-        }
-
-        return getUserRank(userId)
-    }
-
-    async updateUserRanks() {
-        this.users = this.users.map(user => {
-            // TODO: User Rank ermitteln (getUserRankForInternal oder getUserRankForExternal)
-        })
-    }
-
-    getUserRankForInternal(user) {
-        // Irgendwas ermitteln für internen User
-    }
-
-    getUserRankForExternal(user) {
-        // Irgendwas ermitteln für externen User
     }
 }
 
