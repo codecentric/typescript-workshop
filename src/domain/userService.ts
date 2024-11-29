c88ead1089900c72f4eaa0bc1db84427a697baa6
Björn Böing <b.boeing@online.de>
2024-11-29 13:51:05 +0100

    Type userService.ts

--- src/domain/userService.ts
+++ src/domain/userService.ts
@@ -1,22 +1,25 @@
 import { getUserRank } from "./user.js";
 import { loadAllUsers, loadUser } from "../api/user.js";
+import { User } from "../api/types.ts";
 
 export class UserService {
+    users: User[];
+
     constructor() {
         this.users = [];
     }
 
-    async loadAllUsers() {
+    async loadAllUsers(): Promise<User[]> {
         this.users = await loadAllUsers();
 
         return this.users;
     }
 
-    async loadUser(id) {
+    async loadUser(id: number): Promise<User> {
         return loadUser(id);
     }
 
-    async getUserRank(userId) {
+    async getUserRank(userId: number): Promise<string> {
         if (!this.users.includes(userId)) {
             const user = await this.loadUser(userId);
 
