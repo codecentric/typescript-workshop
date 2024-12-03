f97ebea862639208b598b97709a5a638c0b84409
Björn Böing <b.boeing@online.de>
2024-12-03 11:57:33 +0100

    Add setUserType.test.js

---
+++ src/domain/setUserType.test.js
@@ -0,0 +1,9 @@
+describe("setUserType", () => {
+    it("should set user type to EXTERNAL if user has company", () => {
+        // TODO: implement test
+    });
+
+    it("should set user type to INTERNAL if user has no company", () => {
+        // TODO: implement test
+    });
+});
