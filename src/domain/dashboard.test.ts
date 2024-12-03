cd4776c2ed952a2f10b51a1a4630f347940b97b4
Björn Böing <b.boeing@online.de>
2024-12-03 12:02:52 +0100

    Add dashboard.test.ts

---
+++ src/domain/dashboard.test.ts
@@ -0,0 +1,13 @@
+describe("dashboard", () => {
+    describe("prepareDashboard", () => {
+        it("should return dashboard data", () => {
+            // TODO: implement test
+        });
+    });
+
+    describe("updateSinglePost", () => {
+        it("should update single post", () => {
+            // TODO: implement test
+        });
+    });
+});
