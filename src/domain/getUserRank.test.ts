9b0849d39faeb9d0122c401f44a06203461b73cd
Björn Böing <b.boeing@online.de>
2024-12-03 11:59:31 +0100

    Add getUserRank.test.ts

---
+++ src/domain/getUserRank.test.ts
@@ -0,0 +1,13 @@
+describe("getUserRank", () => {
+    it("should return Newbie for user with 0 posts", () => {
+        // TODO: implement test
+    });
+
+    it("should return Beginner for user with 5 posts", () => {
+        // TODO: implement test
+    });
+
+    it("should return Advanced for user with 10 posts", () => {
+        // TODO: implement test
+    });
+});
