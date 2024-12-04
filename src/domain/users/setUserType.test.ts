de56cbe7ec75501ec62ccbebfab27fa651f5fd3d
Björn Böing <b.boeing@online.de>
2024-12-04 14:46:00 +0100

    Add example tests for setUserType.test.ts
    Co-authored-by: CButschkau <christoph.butschkau@codecentric.de>

--- src/domain/users/setUserType.test.ts
+++ src/domain/users/setUserType.test.ts
@@ -1,9 +1,23 @@
+import { setUserType } from "./setUserType.ts";
+import { User } from "../../types/user.ts";
+
+const getTestUser = (partialUser: Partial<User> = {}): User => ({
+    id: 1,
+    name: "John Doe",
+    company: "ACME",
+    ...partialUser,
+});
+
 describe("setUserType", () => {
     it("should set user type to EXTERNAL if user has company", () => {
-        // TODO: implement test
+        const userWithType = setUserType(getTestUser());
+
+        expect(userWithType.type).toBe("EXTERNAL");
     });
 
     it("should set user type to INTERNAL if user has no company", () => {
-        // TODO: implement test
+        const userWithType = setUserType(getTestUser({ company: undefined }));
+
+        expect(userWithType.type).toBe("INTERNAL");
     });
 });
