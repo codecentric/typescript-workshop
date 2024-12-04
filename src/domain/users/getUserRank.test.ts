84aa071f54d4857c48ae95c7b32d4191e5c42518
Björn Böing <b.boeing@online.de>
2024-12-04 14:41:21 +0100

    Add example tests for getUserRank.test.ts
    Co-authored-by: CButschkau <christoph.butschkau@codecentric.de>

--- src/domain/users/getUserRank.test.ts
+++ src/domain/users/getUserRank.test.ts
@@ -1,13 +1,42 @@
+import { getPostsOfUser } from "../../api/post.ts";
+import { Mock } from "vitest";
+import { getUserRank } from "./getUserRank.ts";
+import { UserRank } from "../../types/user.ts";
+import { Post } from "../../types/post.ts";
+
+vi.mock(import("../../api/post.ts"));
+
 describe("getUserRank", () => {
-    it("should return Newbie for user with 0 posts", () => {
-        // TODO: implement test
+    const getPostsOfUserMock = getPostsOfUser as Mock<typeof getPostsOfUser>;
+
+    it("should return Newbie for user with 0 posts", async () => {
+        getPostsOfUserMock.mockResolvedValue([]);
+
+        const rank = await getUserRank(1);
+
+        expect(rank).toBe(UserRank.NEWBIE);
     });
 
-    it("should return Beginner for user with 5 posts", () => {
-        // TODO: implement test
+    it("should return Beginner for user with 4 posts", async () => {
+        getPostsOfUserMock.mockResolvedValue([
+            {} as Post,
+            {} as Post,
+            {} as Post,
+            {} as Post,
+        ]);
+
+        const rank = await getUserRank(1);
+
+        expect(rank).toBe(UserRank.BEGINNER);
     });
 
-    it("should return Advanced for user with 10 posts", () => {
-        // TODO: implement test
+    it("should return Advanced for user with 10 posts", async () => {
+        getPostsOfUserMock.mockResolvedValue(
+            new Array<Post>(10).fill({} as Post),
+        );
+
+        const rank = await getUserRank(1);
+
+        expect(rank).toBe(UserRank.ADVANCED);
     });
 });
