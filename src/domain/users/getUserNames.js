fcdd3bfb13e00962579fcdc673024a047993f93c
Björn Böing <b.boeing@online.de>
2024-12-04 11:12:58 +0100

    Type getUserNames.js

--- src/domain/users/getUserNames.js
+++
@@ -1,7 +0,0 @@
-const mapUserData = (users, mapFunction) => {
-    return users.map(mapFunction);
-};
-
-export const getUserNames = (users) => {
-    return mapUserData(users, (user) => user.name);
-};
---
+++ src/domain/users/getUserNames.ts
@@ -0,0 +1,13 @@
+import { User } from "../../api/types.ts";
+
+const mapUserData = <T>(users: User[], mapFunction: (user: User) => T): T[] => {
+    return users.map(mapFunction);
+};
+
+export const getUserNames = (users: User[]): string[] => {
+    return mapUserData(users, (user) => user.name);
+};
+
+export const getUserIds = (users: User[]): number[] => {
+    return mapUserData(users, (user) => user.id);
+};
